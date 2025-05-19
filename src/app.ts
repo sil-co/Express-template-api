d715236b0f5fd044d9720b9196ac3749696b9f30
shelner <shelner.book@gmail.com>
2025-05-20 08:20:31 +0900

    Fix app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors';
 import itemRoutes from './routes/itemRoutes';
 import { errorHandler } from './middlewares/errorHandler';
 
 const app = express();
+app.use(cors()); // same `app.use(cors({ origin: '*' }));`
 app.use(express.json());
 app.use('/api/items', itemRoutes);
 app.use(errorHandler);
